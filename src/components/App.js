b9f32093e6c7610ceff17095fc5a09c0a87eaa76
wsaldana <53311124+wsaldana@users.noreply.github.com>
2021-03-29 13:14:24 -0600

    React render fixed

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React, {Component} from 'react';
 
 class App extends Component {
     render(){
-        <div>
-            <h1>Memory WS</h1>
-        </div>
+        return(
+            <div>
+                <h1>Memory WS</h1>
+            </div>
+        );
     }
 }
 
