f86a0c03efa19b75b3e708a75ca971d56286bd08
wsaldana <53311124+wsaldana@users.noreply.github.com>
2021-03-29 13:08:41 -0600

    React render

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render(){
+        <div>
+            <h1>Memory WS</h1>
+        </div>
+    }
+}
+
+export default App;
