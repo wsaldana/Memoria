ccd0f53b0e58b884f7ca36494f94db82d951d559
wsaldana <53311124+wsaldana@users.noreply.github.com>
2021-03-30 23:33:13 -0600

    Linting buildDeck

--- src/utils/buildDeck.js
+++ src/utils/buildDeck.js
@@ -1,49 +1,50 @@
+/* eslint-disable linebreak-style */
 import shuffle from 'lodash.shuffle';
-import img1 from '../public/img/1.jpg'
-import img2 from '../public/img/2.jpg'
-import img3 from '../public/img/3.jpg'
-import img4 from '../public/img/4.jpg'
-import img5 from '../public/img/5.jpg'
-import img6 from '../public/img/6.jpg'
-import img7 from '../public/img/7.jpg'
-import img8 from '../public/img/8.jpg'
- 
- const N = 16;
+import img1 from '../public/img/1.jpg';
+import img2 from '../public/img/2.jpg';
+import img3 from '../public/img/3.jpg';
+import img4 from '../public/img/4.jpg';
+import img5 from '../public/img/5.jpg';
+import img6 from '../public/img/6.jpg';
+import img7 from '../public/img/7.jpg';
+import img8 from '../public/img/8.jpg';
+
+const N = 16;
 
 export default () => {
-    let cards = [];
-    let index = 1;
+  const cards = [];
+  let index = 1;
 
-    while(cards.length < N){
-        const img0 = null;
+  while (cards.length < N) {
+    let img0 = null;
 
-        if(index == 1){
-            img0 = img1;
-        } else if(index == 2){
-            img0 = img2;
-        } else if(index == 3){
-            img0 = img3;
-        } else if(index == 4){
-            img0 = img4;
-        } else if(index == 5){
-            img0 = img5;
-        } else if(index == 6){
-            img0 = img6;
-        } else if(index == 7){
-            img0 = img7;
-        } else if(index == 8){
-            img0 = img8;
-        }
-
-        const card = {
-            img: img0,
-            guess: false
-        };
-        cards.push(card);
-        cards.push({...card});
-
-        index++;
+    if (index === 1) {
+      img0 = img1;
+    } else if (index === 2) {
+      img0 = img2;
+    } else if (index === 3) {
+      img0 = img3;
+    } else if (index === 4) {
+      img0 = img4;
+    } else if (index === 5) {
+      img0 = img5;
+    } else if (index === 6) {
+      img0 = img6;
+    } else if (index === 7) {
+      img0 = img7;
+    } else if (index === 8) {
+      img0 = img8;
     }
 
-    return shuffle(cards);
-}
+    const card = {
+      img: img0,
+      guess: false,
+    };
+    cards.push(card);
+    cards.push({ ...card });
+
+    index += 1;
+  }
+
+  return shuffle(cards);
+};
