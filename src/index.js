03a84c634507bc17329263a638e688f601f28d14
wsaldana <53311124+wsaldana@users.noreply.github.com>
2021-03-30 23:29:37 -0600

    Linting index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable linebreak-style */
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App';
 
+// eslint-disable-next-line react/jsx-filename-extension
 ReactDom.render(<App />, document.getElementById('app'));
